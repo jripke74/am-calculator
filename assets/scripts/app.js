8a3c5115899bc77d62966a8dbea8a965cb45d3e0
jripke74 <jripke74@gmail.com>
2025-10-30 10:16:31 -0700

    add * and /

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -36,9 +36,15 @@ function calculateResult(calculationType) {
   if (calculationType === 'ADD') {
     currentResult += enteredNumber;
     mathOperator = "+";
-  } else {
+  } else if (calculationType === 'SUBTRACT') {
     currentResult -= enteredNumber;
     mathOperator = "-";
+  } else if (calculationType === 'MULTIPLY') {
+    currentResult *= enteredNumber;
+    mathOperator = "*";
+  } else if (calculationType === 'DIVIDE') {
+    currentResult /= enteredNumber;
+    mathOperator = "/"
   }
   currentResult += enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
@@ -54,19 +60,11 @@ function subtract() {
 }
 
 function multiply() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult *= enteredNumber;
-  createAndWriteOutput('*', initialResult, enteredNumber);
-  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+  calculateResult('MULTIPLY');
 }
 
 function divide() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult /= enteredNumber;
-  createAndWriteOutput('/', initialResult, enteredNumber);
-  writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+  calculateResult('DIVIDE');
 }
 
 addBtn.addEventListener('click', add);
